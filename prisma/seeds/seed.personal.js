2b744fcede0454bd46209b46acf72541ab4d371a
Alexis Sushko <alexis.sushko@gmail.com>
2024-12-02 11:57:48 -0500

    Added grocery lists into database

--- prisma/seeds/seed.personal.js
+++ prisma/seeds/seed.personal.js
@@ -13,6 +13,91 @@ const seedPersonal = async (numPasswords = 5) => {
   await prisma.personalPassword.createMany({
     data: passwords,
   });
+
+  //Create data for grocery lists
+  const groceryListData = [
+    {
+      userId: 1,
+      itemName: "Eggs",
+      quantity: 2,
+      isPurchased: false,
+    },
+    {
+      userId: 1,
+      itemName: "Almond Milk",
+      quantity: 1,
+      isPurchased: false,
+    },
+    {
+      userId: 2,
+      itemName: "Blackberries",
+      quantity: 4,
+      isPurchased: true,
+    },
+    {
+      userId: 2,
+      itemName: "Raspberries",
+      quantity: 1,
+      isPurchased: false,
+    },
+    {
+      userId: 2,
+      itemName: "Tofu",
+      quantity: 1,
+      isPurchased: false,
+    },
+    {
+      userId: 2,
+      itemName: "Bananas",
+      quantity: 2,
+      isPurchased: true,
+    },
+    {
+      userId: 3,
+      itemName: "Broccoli",
+      quantity: 2,
+      isPurchased: false,
+    },
+    {
+      userId: 3,
+      itemName: "Muffins",
+      quantity: 1,
+      isPurchased: true,
+    },
+    {
+      userId: 3,
+      itemName: "Kcups",
+      quantity: 1,
+      isPurchased: true,
+    },
+    {
+      userId: 4,
+      itemName: "Pears",
+      quantity: 2,
+      isPurchased: true,
+    },
+    {
+      userId: 4,
+      itemName: "Clementines",
+      quantity: 1,
+      isPurchased: false,
+    },
+    {
+      userId: 5,
+      itemName: "English Muffins",
+      quantity: 1,
+      isPurchased: true,
+    },
+    {
+      userId: 5,
+      itemName: "Grapes",
+      quantity: 1,
+      isPurchased: false,
+    },
+  ];
+
+  //Insert grocery lists into datanbase
+  await prisma.groceryList.createMany({ data: groceryListData });
 };
 
 module.exports = seedPersonal;
