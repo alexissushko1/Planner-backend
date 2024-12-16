6946dc08a4ab63f3c4455c781bad9fed36e4d268
Alexis Sushko <alexis.sushko@gmail.com>
2024-12-16 16:32:05 -0500

    Created spending routes

--- api/personalApi/personalFinances.js
+++ api/personalApi/personalFinances.js
@@ -78,4 +78,80 @@ router.delete("/budget/:id", authenticate, async (req, res, next) => {
   }
 });
 
+//Get all spending entries
+router.get("/personal-spending", authenticate, async (req, res, next) => {
+  try {
+    const spending = await prisma.spendTracker.findMany();
+    res.json(spending);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Add a spending entry
+router.post("/personal-spending", authenticate, async (req, res, next) => {
+  const { userId, description, amount } = req.body;
+  try {
+    const spending = await prisma.spendTracker.create({
+      data: {
+        userId,
+        description,
+        amount,
+      },
+    });
+    res.status(201).json(spending);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Update a spending entry
+router.patch("/personal-spending/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+  const { userId, description, amount } = req.body;
+
+  try {
+    const spending = await prisma.spendTracker.findUniqueOrThrow({
+      where: { id: +id },
+    });
+    if (!spending) {
+      return next({
+        status: 404,
+        message: `Spending entry ${id} does not exist`,
+      });
+    }
+
+    const updateData = {};
+    if (userId) updateData.userId = +userId;
+    if (description) updateData.description = description;
+    if (amount) updateData.amount = amount;
+
+    const updatedSpending = await prisma.spendTracker.update({
+      where: { id: +id },
+      data: updateData,
+    });
+    res.json(updatedSpending);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Delete a spending entry
+router.delete(
+  "/personal-spending/:id",
+  authenticate,
+  async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const spending = await prisma.spendTracker.findUniqueOrThrow({
+        where: { id: +id },
+      });
+      await prisma.spendTracker.delete({ where: { id: +id } });
+      res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
 module.exports = router;
