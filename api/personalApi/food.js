fea7b3c8a847f2eb1ab3e06994ccb7dc255efba4
Alexis Sushko <alexis.sushko@gmail.com>
2024-12-16 16:01:24 -0500

    Created weakly meals routes

--- api/personalApi/food.js
+++ api/personalApi/food.js
@@ -77,4 +77,77 @@ router.delete("/grocery/:id", authenticate, async (req, res, next) => {
     next(e);
   }
 });
+
+//Get all meals for the week
+router.get("/weekly-meals", authenticate, async (req, res, next) => {
+  try {
+    const meals = await prisma.mealsForWeek.findMany();
+    res.json(meals);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Add a meal
+router.post("/weekly-meals", authenticate, async (req, res, next) => {
+  const { userId, dayOfWeek, mealDescription } = req.body;
+  try {
+    const meals = await prisma.mealsForWeek.create({
+      data: {
+        userId,
+        dayOfWeek,
+        mealDescription,
+      },
+    });
+    res.status(201).json(meals);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Update a meal
+router.patch("/weekly-meals/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+  const { userId, dayOfWeek, mealDescription } = req.body;
+
+  try {
+    const meals = await prisma.mealsForWeek.findUniqueOrThrow({
+      where: { id: +id },
+    });
+    if (!meals) {
+      return next({
+        status: 404,
+        message: `Meal ${id} does not exist`,
+      });
+    }
+
+    const updateData = {};
+    if (userId) updateData.userId = +userId;
+    if (dayOfWeek) updateData.dayOfWeek = dayOfWeek;
+    if (mealDescription) updateData.mealDescription = mealDescription;
+
+    const updatedMeals = await prisma.mealsForWeek.update({
+      where: { id: +id },
+      data: updateData,
+    });
+    res.json(updatedMeals);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Delete a meal
+router.delete("/weekly-meals/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const meals = await prisma.mealsForWeek.findUniqueOrThrow({
+      where: { id: +id },
+    });
+    await prisma.mealsForWeek.delete({ where: { id: +id } });
+    res.sendStatus(204);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
